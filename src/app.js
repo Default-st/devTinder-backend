c60eadd19be212ff9682d57d1713291ac0f0f2b5
Default-st <stnshsrivastava@gmail.com>
2025-06-04 21:38:27 +0530

    JWT and Cookies

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import { connectDB } from "./config/database.js";
 import { User } from "./models/user.js";
 import { validateSignUpData, validateLoginData } from "./utils/validation.js";
 import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the User model
@@ -44,12 +47,37 @@ app.post("/login", async (req, res) => {
 
     const isPasswordValid = await bcrypt.compare(password, userData.password);
     if (isPasswordValid) {
+      // Create a JWT Token
+      const token = await jwt.sign({ _id: userData._id }, "DEV@Tinder$790");
+
+      // Add the token to cookie and send the response back to the server
+      res.cookie("token", token);
+
       res.send("Login Successful");
     } else {
       throw new Error("Email ID or Password is incorrect");
     }
   } catch (error) {
     res.status(400).send("Error signing up the user: " + error.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
+
+    const user = await User.findById(decodedMessage._id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
