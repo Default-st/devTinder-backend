c37f2905b92cb87017b3712d1d8a194496aea627
Default-st <stnshsrivastava@gmail.com>
2025-06-05 10:36:42 +0530

    Episode 12 completed

--- APIList.md
+++ APIList.md
@@ -14,7 +14,7 @@ PATCH /profile/password
 
 # connectionRequestRouter
 
-POST /request/send/intrested/:userId
+POST /request/send/interested/:userId
 POST /request/send/ignored/:userId
 POST /request/review/accepted/:requestId
 POST /request/review/rejected/:requestId
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,9 +1,56 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
+import { ConnectionRequest } from "../models/connectionRequest.js";
+import { User } from "../models/user.js";
 
 export const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, (req, res) => {
-  const user = req.user;
-  res.send("Connection Request sent by " + user.firstName);
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"].includes(status);
+
+      if (!allowedStatus) {
+        return res
+          .status(400)
+          .json({ message: "Invalid Status Type " + status });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(400).json({ message: "User not Found" });
+      }
+
+      const connectionRequestAlreadyExist = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (connectionRequestAlreadyExist) {
+        throw new Error("Connection Request already exists");
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: req.user.firstName + " " + status + " " + toUser.firstName,
+        data: data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR: " + error.message);
+    }
+  }
+);
